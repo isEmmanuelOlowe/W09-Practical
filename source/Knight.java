3833251f2771794eb845da1cd92203309afe991a
eo32 <eo32@pc5-022-l.cs.st-andrews.ac.uk>
2018-11-16 19:20:59 +0000

    Knight Piece bug fixed

--- source/Knight.java
+++ source/Knight.java
@@ -3,7 +3,7 @@ import java.lang.Math;
 class Knight extends Piece {
   private String piece_name = "KNIGHT";
   private char[] color = {'♘', '♞'};
-  
+
   //constructor for Knight
   public Knight(int playerNo, Board gameBoard, String location){
     //constructor of super class is called
@@ -16,10 +16,10 @@ class Knight extends Piece {
 
   public boolean validDesination(int[] from, int[] to){
     //determines if the location that is requested can be reached by night
-    if(Math.abs(to[0] - from[0]) == 2 || Math.abs(to[1]- from[1]) == 1){
+    if(Math.abs(to[0] - from[0]) == 2 && Math.abs(to[1]- from[1]) == 1){
       return true;
     }
-    if(Math.abs(to[1] - from[1]) == 2 || Math.abs(to[0]- from[0]) == 1){
+    if(Math.abs(to[1] - from[1]) == 2 && Math.abs(to[0]- from[0]) == 1){
       return true;
     }
     return false;
